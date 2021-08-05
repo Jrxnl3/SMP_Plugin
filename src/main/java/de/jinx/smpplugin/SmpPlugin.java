476d772f6a1a1b233cd1cd169cb56acff9372ea2
jinx_dev <benhoffe1398@gmail.com>
2021-08-06 01:10:06 +0200

    Added Rotten Flesh burning recipe!

--- src/main/java/de/jinx/smpplugin/SmpPlugin.java
+++ src/main/java/de/jinx/smpplugin/SmpPlugin.java
@@ -1,6 +1,11 @@
 package de.jinx.smpplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.SmokingRecipe;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,6 +26,12 @@ public final class SmpPlugin extends JavaPlugin {
 
         this.getCommand("broadcast").setExecutor(new Commands());
         this.getCommand("rename").setExecutor(new Commands());
+
+        FurnaceRecipe furnaceRecipe = new FurnaceRecipe(NamespacedKey.fromString("smpplugin"),new ItemStack(Material.LEATHER), Material.ROTTEN_FLESH,1,20*30);
+        SmokingRecipe leatherSmoker = new SmokingRecipe(NamespacedKey.fromString("smpplugin"),new ItemStack(Material.LEATHER), Material.ROTTEN_FLESH,1,20*20);
+
+        getServer().addRecipe(furnaceRecipe);
+        getServer().addRecipe(leatherSmoker);
     }
 
     @Override
